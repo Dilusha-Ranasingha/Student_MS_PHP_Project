8a032493c25d64e6d9e53574a46d9d91080a5472
IT22171092RANDRanasingha <dilushar10@gmail.com>
2024-07-14 20:54:07 +0530

    build: update signup validation

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 <body>
     <fieldset>
         <legend><b>SIGN-UP</b></legend>
-        <form method="post" action="signup_php.php" id="signupForm">
+        <form method="post" action="signup_php.php" id="signupForm" onsubmit="return validateForm();">
             ID : <input type="text" disabled><br>
             First Name : <input type="text" name="fname"><br>
             Last Name : <input type="text" name="lname"><br>
@@ -14,11 +14,65 @@
             Email : <input type="email" name="email"><br>
             Password : <input type="password" name="password"><br>
 
-            <input type="submit" value="Signup">
+            <button type="submit" >Sign in</button>
             <input type="reset">
 
         </form>
     </fieldset>
+    
+    <script>
+        function validateForm() {
+            var fname = document.forms["signupForm"]["fname"].value;
+            var lname = document.forms["signupForm"]["lname"].value;
+            var age = document.forms["signupForm"]["age"].value;
+            var dob = document.forms["signupForm"]["dob"].value;
+            var contact = document.forms["signupForm"]["contact"].value;
+            var email = document.forms["signupForm"]["email"].value;
+            var password = document.forms["signupForm"]["password"].value;
+
+            var nameRegex = /^[A-Za-z\s]+$/;
+            var numberRegex = /^[0-9]+$/;
+            var emailRegex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]+$/i;
+            var passwordRegex = /^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+
+            if (fname == "" || lname == "" || age == "" || dob == "" || contact == "" || email == "" || password == "") {
+                alert("All fields must be filled out");
+                return false;
+            }
+
+            if (!nameRegex.test(fname)) {
+                alert("First name should contain only letters and spaces");
+                return false;
+            }
+
+            if (!nameRegex.test(lname)) {
+                alert("Last name should contain only letters and spaces");
+                return false;
+            }
+
+            if (!numberRegex.test(age)) {
+                alert("Age should contain only numbers");
+                return false;
+            }
+
+            if (!numberRegex.test(contact)) {
+                alert("Contact number should contain only numbers");
+                return false;
+            }
+
+            if (!email.endsWith("@gmail.com")) {
+                alert("Email should end with @gmail.com");
+                return false;
+            }
+
+            if (!passwordRegex.test(password) || /\s/.test(password)) {
+                alert("Password should contain only letters, numbers, and symbols, and should not contain spaces");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
     
 </body>
 </html>
